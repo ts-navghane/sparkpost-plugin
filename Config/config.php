18c737e0c1339b644482dc67067a831193bbc42d
Tejas Navghane <ts.navghane@gmail.com>
2023-06-08 17:00:29 +0530

    Initial commit

---
+++ Config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name'        => 'Sparkpost Plugin',
+    'description' => 'Sparkpost Mailer Plugin for Mautic',
+    'version'     => '1.0.0',
+    'author'      => 'Acquia',
+    'routes'      => [
+        'main'   => [],
+        'public' => [],
+        'api'    => [],
+    ],
+    'menu'        => [],
+    'services'    => [
+        'other'        => [],
+        'sync'         => [],
+        'integrations' => [],
+    ],
+];
---
+++ README.md
@@ -0,0 +1,3 @@
+### Mautic Sparkpost Plugin
+
+This plugin enable Mautic 5 to run Sparkpost as a transport.
